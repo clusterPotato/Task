53cd0a4c101b24b1af05c7c938a55c7642467c14
Gavin Craft <gavincraft2016@gmail.com>
2021-04-23 12:14:22 -0600

    fixed thing

---
+++ Task/Task/Models/Task.swift
@@ -0,0 +1,29 @@
+//
+//  Task.swift
+//  Task
+//
+//  Created by Gavin Craft on 4/21/21.
+//
+
+import Foundation
+
+class Task: Codable {
+    var name: String
+    var notes: String?
+    var dueDate: Date?
+    var isComplete: Bool
+    var uuid: String
+    
+    init(name: String, notes: String?, dueDate: Date?, isComplete: Bool = false, uuid: String = UUID().uuidString) {
+        self.name = name
+        self.notes = notes
+        self.dueDate = dueDate
+        self.isComplete = isComplete
+        self.uuid = uuid
+    }
+}//End of class
+extension Task: Equatable {
+    static func == (lhs: Task, rhs: Task) -> Bool {
+        return lhs.uuid == rhs.uuid
+    }
+}
